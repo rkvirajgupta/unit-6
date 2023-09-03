9f756f31b6a8f87850f0646d146ed1a56b6520ed
Viraj Gupta <vgupta2@q3infotech.net>
2023-09-03 22:56:46 +0530

    fix: hide secret sources

--- npl_asg/backend/src/configs/db.js
+++ npl_asg/backend/src/configs/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connect = () => {
-  return mongoose.connect("mongodb+srv://virajgupta:Virajgupta12345@cluster0.wmplk.mongodb.net/npl?retryWrites=true&w=majority");
+  return mongoose.connect(`mongodb+srv://${evn.SECRET_KEY}@cluster0.wmplk.mongodb.net/npl?retryWrites=true&w=majority`);
 };
 
 module.exports= connect;
--- npl_asg_1/backend/src/config/db.js
+++ npl_asg_1/backend/src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connect = () => {
-  return mongoose.connect("mongodb+srv://virajgupta:Virajgupta12345@cluster0.wmplk.mongodb.net/npl_asg_1?retryWrites=true&w=majority");
+  return mongoose.connect(`mongodb+srv://${evn.SECRET_KEY}@cluster0.wmplk.mongodb.net/npl_asg_1?retryWrites=true&w=majority`);
 };
 
 module.exports= connect;
